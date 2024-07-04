f797a3b31fe65272802eb4d9268da29531583c5f
gabriele43 <162490335+gabriele43@users.noreply.github.com>
2024-07-04 16:45:25 -0300

    Add files via upload

---
+++ jogo.ts
@@ -0,0 +1,12 @@
+class jogo{
+    titulo:string
+    genero:string
+    classificaçaoetaria:number
+
+    constructor(titulo:string,genero:string,classificaçaoetaria:number){
+        this.titulo = titulo
+        this.genero = genero
+        this.classificaçaoetaria = classificaçaoetaria
+
+    }
+}
